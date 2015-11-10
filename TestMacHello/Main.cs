6f9cc93925fbc8c8deba79e79c2c8f473f44a3d3
saurabhd@360logica.com <saurabhd@360logica.com>
2015-11-10 12:55:12 +0530

    Initial check-in of module TestMacHello

---
+++ TestMacHello/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace TestMacHello
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
